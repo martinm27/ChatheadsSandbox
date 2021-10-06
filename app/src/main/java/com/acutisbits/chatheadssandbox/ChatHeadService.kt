70f1184a10b05dbc9e0a2aec55f8385a80673381
Martin Mihalic <mihamac23@gmail.com>
2021-10-06 20:24:27 +0200

    Fixing build

--- app/src/main/java/com/acutisbits/chatheadssandbox/ChatHeadService.kt
+++ app/src/main/java/com/acutisbits/chatheadssandbox/ChatHeadService.kt
@@ -9,14 +9,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.lifecycle.LifecycleService
 
 class ChatHeadService : LifecycleService() {
 
     private var windowManager: WindowManager? = null
     private var chatHeadView: View? = null
-    private var statusBarView: View? = null
 
     @SuppressLint("InflateParams", "ClickableViewAccessibility")
     override fun onCreate() {
@@ -24,7 +22,31 @@ class ChatHeadService : LifecycleService() {
 
         chatHeadView = LayoutInflater.from(this).inflate(R.layout.chat_head_layout, null)
 
-        val chatHeadParams = WindowManager.LayoutParams(
+        val chatHeadParams = buildChatHeadWindowParams()
+
+        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager?
+        windowManager?.addView(chatHeadView, chatHeadParams)
+
+        //Drag and move chat head using user's touch action.
+        val chatHeadImage = chatHeadView?.findViewById(R.id.chat_head_profile_iv) as ImageView
+        chatHeadImage.setOnTouchListener(
+            ChatHeadTouchListener(
+                chatHeadView!!,
+                windowManager!!,
+                chatHeadParams
+            ) {
+                val intent = Intent(this, MainActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+
+                startActivity(intent)
+                stopSelf()
+            }
+        )
+    }
+
+    private fun buildChatHeadWindowParams(): WindowManager.LayoutParams =
+        WindowManager.LayoutParams(
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.WRAP_CONTENT,
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY else WindowManager.LayoutParams.TYPE_PHONE,
@@ -36,34 +58,8 @@ class ChatHeadService : LifecycleService() {
             y = 200
         }
 
-        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager?
-        windowManager?.addView(chatHeadView, chatHeadParams)
-
-        //Drag and move chat head using user's touch action.
-        val chatHeadImage = chatHeadView?.findViewById(R.id.chat_head_profile_iv) as ImageView
-        chatHeadImage.setOnTouchListener(ChatHeadTouchListener(
-            chatHeadView!!,
-            windowManager!!,
-            chatHeadParams
-        ) {
-            //Open the chat conversation click.
-            val intent = Intent(this, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
-
-            startActivity(intent)
-            stopSelf()
-        })
-
-        val chatHeadTimeText = chatHeadView?.findViewById<TextView>(R.id.chat_head_call_time)
-        IncomingCallTimeUtility.incomingCallTime().observe(this, {
-            chatHeadTimeText?.text = it
-        })
-    }
-
     override fun onDestroy() {
         if (chatHeadView != null) windowManager?.removeView(chatHeadView)
-        if (statusBarView != null) windowManager?.removeView(statusBarView)
         super.onDestroy()
     }
 }
--- app/src/main/java/com/acutisbits/chatheadssandbox/MainActivity.kt
+++ app/src/main/java/com/acutisbits/chatheadssandbox/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.acutisbits.chatheadssandbox
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.util.*
 
 const val ONE_SECOND_IN_MILLISECONDS = 1000L
@@ -25,25 +27,38 @@ class MainActivity : AppCompatActivity() {
         //Check if the application has draw over other apps permission or not?
         //This permission is by default available for API<23. But for API > 23
         //you have to ask for the permission in runtime.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
 
-            //If the draw over permission is not available open the settings screen
-            //to grant the permission.
-            val activityResultRegistration = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-                //Check if the permission is granted or not.
-                if (it) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            when {
+                ContextCompat.checkSelfPermission(this, Settings.ACTION_MANAGE_OVERLAY_PERMISSION) == PackageManager.PERMISSION_GRANTED -> {
                     initializeView()
-                } else { //Permission is not available
-                    Toast.makeText(
-                        this,
-                        "Draw over other app permission not available. Closing the application",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    finish()
+                }
+                ActivityCompat.shouldShowRequestPermissionRationale(this, Settings.ACTION_MANAGE_OVERLAY_PERMISSION) -> {
+                    // In an educational UI, explain to the user why your app requires this
+                    // permission for a specific feature to behave as expected. In this UI,
+                    // include a "cancel" or "no thanks" button that allows the user to
+                    // continue using your app without granting the permission.
+                }
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    val uri: Uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
                 }
             }
-            activityResultRegistration.launch(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
         } else {
+            initializeView()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
             initializeView()
         }
     }
@@ -63,6 +78,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<View>(R.id.end_call).setOnClickListener {
+            stopService(Intent(this@MainActivity, ChatHeadService::class.java))
             IncomingCallTimeUtility.reset()
         }
     }
